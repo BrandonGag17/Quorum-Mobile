3ebc2e4ed99362abd60c279271eb6d785f06c82f
Android Studio <devandroid@ort>
2026-05-18 12:10:02 -0300

    botones del loginup e introduccion

--- Quorum/App.js
+++ Quorum/App.js
@@ -3,13 +3,8 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { useFonts } from 'expo-font';
 
 import Introduccion from './src/Introduccion'
-// import SignUp from './src/SignUp'
 import LogIn from './src/LogIn'
-// import Inicio from './src/Inicio'
-// import Configuracion from './src/Configuracion'
 import Inicio from './src/Inicio'
-// import ProponerJuntada from './src/ProponerJuntada'
-// import DetallePerfil from './src/DetallePerfil'
 
 const Stack = createNativeStackNavigator()
 
@@ -25,13 +20,8 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Introduccion">
         <Stack.Screen name="Introduccion" component={Introduccion} />
-        {/* <Stack.Screen name="SignUp" component={SignUp} /> */}
         <Stack.Screen name="LogIn" component={LogIn} />
-        {/* <Stack.Screen name="Inicio" component={Inicio} /> */}
-        {/* <Stack.Screen name="Configuracion" component={Configuracion} /> */}
         <Stack.Screen name="Inicio" component={Inicio} />
-        {/* <Stack.Screen name="ProponerJuntada" component={ProponerJuntada} /> */}
-        {/* <Stack.Screen name="DetallePerfil" component={DetallePerfil} /> */}
       </Stack.Navigator>
     </NavigationContainer>
   )
